5d5dc86df89c4a9ce4a621eeb3c64bfce9a955d9
Stefan Grönke <stefan@gronke.net>
2015-11-13 01:16:38 +0100

    setup test suite

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,23 +2,30 @@ var path = require('path'),
 	fs = require('fs'),
 	gulp = require('gulp'),
 	rmdir = require('rmdir'),
+	mkdirp = require('mkdirp'),
 	merge = require('merge2'),
+	runSequence = require('run-sequence'),
 	gulpLoadPlugins = require('gulp-load-plugins');
 
 var plugins = gulpLoadPlugins();
 
 var config = {
 	src: './src',
-	dist: './dist'
+	dist: './dist',
+	test: './test'
 };
 
 gulp.task('clean', function() {
 	return new Promise(function(resolve, reject) {
+		function done() {
+			mkdirp(config.dist);
+			resolve();
+		}
 		try {
 			if(fs.existsSync(config.dist)) {
-				rmdir(config.dist, resolve);
+				rmdir(config.dist, done);
 			} else {
-				resolve();
+				done();
 			}
 		} catch(e) {
 			reject(e);
@@ -40,4 +47,21 @@ gulp.task('typescript', function() {
 
 });
 
-gulp.task('default', ['clean', 'typescript']);
+gulp.task('mocha', function() {
+	return gulp.src(path.join(config.test, 'runner.html'), {
+		read: false
+	}).pipe(plugins.mochaPhantomjs({
+		reporter: 'spec',
+		phantomjs: {
+			useColors: true
+		}
+	}));
+});
+
+gulp.task('test', function(cb) {
+	return runSequence('build', 'mocha', cb);
+});
+gulp.task('build', function(cb) {
+	return runSequence('clean', 'typescript', cb);
+});
+gulp.task('default', ['build']);
